2cea7c4f2a8eee86910db8778b01d6436c6cad5d
Z-Najdaoui <zobirnajdaoui@gmail.com>
2023-10-15 00:17:14 +0100

    setup todolist web app

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { Theme } from '@radix-ui/themes'
+import './index.css'
+import '@radix-ui/themes/styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Theme appearance="dark">
+      <App />
+    </Theme>
+  </React.StrictMode>,
+)
