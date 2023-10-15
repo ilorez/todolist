4e04e5651c0d5bc425788cc351e6e325dc4c57a8
Z-Najdaoui <zobirnajdaoui@gmail.com>
2023-10-15 02:46:45 +0100

    add tailwind color_config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': '#EDEDED',
+        'background-light': '#F9F7F7',
+        'dark-blue': '#1C274C',
+        'tdl_main': {
+          'blue': '#0095FF',
+          'green': '#27C289',
+          'red': '#FF6E0A',
+          'brown': 'DD894F'
+        }
+      }
+    },
+
   },
   plugins: [],
 }
